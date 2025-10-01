2e918c61e3d4ee04e817fc1f4a876e088eed4d24
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-10-01 17:50:46 +0200

    added data analysis and visualization

--- python/scikitLearnPrediction.py
+++ python/scikitLearnPrediction.py
@@ -6,4 +6,20 @@ import sys
 #wypisywanie danych
 df = pd.read_csv('CarPrice_Assignment.csv', delim_whitespace=False, header=0)
 #print(df.head())
+#print(df.columns)
+
+
+#analiza danych
+#print(df.describe()) #statystyki opisowe
+print(df.info()) #typy danych
+
+
+#wizualizacja danych 
+
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+col_study = ['price', 'horsepower']
+sns.pairplot(df[col_study])
+plt.show()
 
