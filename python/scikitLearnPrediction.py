dfd98e008e0c4f7229a7647d6553cbfcfaabb973
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-10-01 17:35:58 +0200

    added dataset

---
+++ python/scikitLearnPrediction.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import sys
+
+#wypisywanie danych
+df = pd.read_csv('CarPrice_Assignment.csv', delim_whitespace=False, header=0)
+#print(df.head())
+
