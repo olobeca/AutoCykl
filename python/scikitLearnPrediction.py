38e9e181fbd6e4ea924378c7b43e3663b3b305f8
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-10-05 08:30:55 +0200

    added 2d model price/horsepower

--- python/scikitLearnPrediction.py
+++ python/scikitLearnPrediction.py
@@ -11,7 +11,7 @@ df = pd.read_csv('CarPrice_Assignment.csv', delim_whitespace=False, header=0)
 
 #analiza danych
 #print(df.describe()) #statystyki opisowe
-print(df.info()) #typy danych
+# print(df.info()) #typy danych
 
 
 #wizualizacja danych 
@@ -23,13 +23,44 @@ import seaborn as sns
 #sns.pairplot(df[col_study])
 #plt.show()
 
-pd.options.display.float_format = '{:,.4f}'.format 
+# # pd.options.display.float_format = '{:,.4f}'.format 
+# #print(df.corr()) 
+# plt.figure(figsize=(12,8))
+# sns.heatmap(df.corr(), annot=True, fmt=".2f")
+# plt.show()
+# nie ma sensu tego pokazaywac bo w danych sa stringi- nazwy aut itp
 
 
-print(df.corr()) 
+
+# sprawdzenie czy dane nie sa puste 
+# print(df.isnull().sum()) 
+
+
+
+# test danych 
+
+y = df['price'].values
+# print(y)
+X  = df[['horsepower']].values #podwojne nawiasy zamiast reshape 
+# print(X)
+
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+model.fit(X,y)
+
+model.coef_ #wspolczynnik kierunkowy
+model.intercept_ #wyraz wolny 
+
+
 plt.figure(figsize=(12,8))
-sns.heatmap(df.corr(), annot=True, fmt=".2f")
+sns.regplot(data=df, x='horsepower', y='price')
+plt.xlabel('horsepower')
+plt.ylabel("price")
 plt.show()
 
-
+from sklearn.metrics import r2_score
+print(r2_score(y, model.predict(X)))
+#jakosc modelu 
 
