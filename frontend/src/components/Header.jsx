552e411dd65035390a9d4a6c28a674e0efa4f039
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-10-25 12:58:58 +0200

    added atiliwnd

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function Header() {
+  return (
+    <>
+      <h1 className="text-clip">Car Price Predictor</h1> 
+      <p>Welcome to the Car Price Predictor application!</p>
+
+    </>
+  );
+}
+
+export default Header;
