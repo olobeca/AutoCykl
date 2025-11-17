e131cfbfbcd97b31042a74b1121f98229707120b
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-11-17 21:54:38 +0100

    added database routes

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ try {
 const userRoutes = require("./routes/userRoutes");
 app.use("/api/users", userRoutes);
 
+const databaseRoutes = require("./routes/databaseRoutes");
+app.use("/database", databaseRoutes);
+
 // Start serwera
 const PORT = 5001;
 app.listen(PORT, () => {
