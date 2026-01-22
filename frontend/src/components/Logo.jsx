539d31f991175b149bda8a11a94490d3463e4100
Aleksander Radecki <alerksanderradecki@gmail.com>
2026-01-22 08:49:40 +0100

    changed logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,13 +1,15 @@
 import "../App.css";
-import car from '../car.png';
+// import car from '../car.png';
+import car from "../icons/car.svg";
 import { useNavigate } from "react-router-dom";
 
 function Logo() {
     const navigate = useNavigate();
     return (
         <div className='flex items-center gap-2' onClick={() => navigate("/")}>
-            <img className="h-16 w-16 cursor-pointer" src={car} alt="Logo" />
-            <span className="text-2xl font-normal text-gray-900 cursor-pointer">AutoPortal</span>
+            {/* <img className="h-16 w-16 cursor-pointer" src={car} alt="Logo" /> */}
+            <img src={car} alt="Logo" className="w-12 h-12 p-2 bg-orange-600 rounded-full" />
+            <span className="text-2xl font-normal text-gray-900 cursor-pointer">AutoCykl</span>
           </div> 
     )
 }
